7bb1aec11710984820bbb86a547c2ac744cfbbbf
hojinlee <infohojin@gmail.com>
2018-06-10 04:31:43 +0900

    init

---
+++ src/component.php
@@ -0,0 +1,33 @@
+<?php
+namespace Jiny\Menu;
+
+use \Jiny\Core\Registry;
+/**
+ * 메뉴 항목구성
+ */
+abstract class Component
+{
+    private $_name;
+    private $_link;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->_name = $name;
+    }
+
+    public function getLink()
+    {
+        return $this->link;
+    }
+
+    public function setLink($link)
+    {
+        $this->_link = $link;
+    }
+
+}
---
+++ src/composite.php
@@ -0,0 +1,31 @@
+<?php
+namespace Jiny\Menu;
+
+use \Jiny\Core\Registry;
+/**
+ * 디렉토리 설정
+ */
+class Composite extends Component
+{
+    public $_children;
+
+    public function __construct($name)
+    {
+        //echo __CLASS__."가 생성이 되었습니다.<br>";
+        $this->setName($name);
+    }
+
+    public function addNode(component $folder)
+    {
+        // 배열 원소 가합니다.
+        $name = $folder->getName();
+        //echo "폴더 ".$name."를 추가합니다.<br>";
+        $this->_children[$name] = $folder;
+    }
+
+    public function removeNode($component)
+    {
+        // 배열 원소를 제거합니다.
+    }
+
+}
